2d8d152e36bd583050d470cb4bb40816ab1debc8
chaos29092 <chaos29092@gmail.com>
2014-11-30 20:45:57 +0800

    11-30 fix

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -14,65 +14,35 @@
 <div class="container">
 
 <div class="row">
-
     <div class="bottom-padding col-sm-6 col-md-6">
-        <img src="img/tianci.jpg" class="img-responsive img-rounded center-block" alt=""/>
+        <h4>遗传图谱</h4>
+
+        <p class="lead">右图如果有人发现了某种遗传疾病， <br/>
+        你知道哪些人会影响到你或者你的孩子吗？</p>
+
     </div>
 
     <div class="bottom-padding col-sm-6 col-md-6">
-        <h4>该是什么就是什么</h4>
-        <p>答案： <strong>6个</strong></p>
-
-        <p>采集基因样本，进行基因筛查，出具基因报告，专业解读报告，并提供专家指导服务。</p>
-        <p>受检者收到基因报告后，由专业老师为受检者提供一对一的报告讲解：让受检者更清晰的了解自己的遗传体质特征；不同的年龄段健康或教育的重点在哪里；具体应怎样执行，如饮食怎样调理，怎样加强保健，怎样教育宝宝等。具体内容视受检者所选筛查项而定。</p>
-        <h4>广告词</h4>
-        <p>收到基因报告后，由检测机构为受检者提供为期一年的免费服务，具体内容视筛查项目而定。包括：</p>
-
-        <a href="{{asset('process')}}"><button type="button" class="btn btn-primary">查看检测流程</button></a>
+        <img src="img/jiapu.jpg" class="img-responsive img-rounded center-block" alt=""/>
     </div>
 </div>
 </div>
 
-<div class="container" id="duyi">
-<div class="row" >
-    <div class="bottom-padding col-sm-6 col-md-6">
-        <h4>基因的独一无二性</h4>
-        <p>答案： <strong>6个</strong></p>
-
-        <p>采集基因样本，进行基因筛查，出具基因报告，专业解读报告，并提供专家指导服务。</p>
-        <p>受检者收到基因报告后，由专业老师为受检者提供一对一的报告讲解：让受检者更清晰的了解自己的遗传体质特征；不同的年龄段健康或教育的重点在哪里；具体应怎样执行，如饮食怎样调理，怎样加强保健，怎样教育宝宝等。具体内容视受检者所选筛查项而定。</p>
-        <h4>广告词</h4>
-        <p>收到基因报告后，由检测机构为受检者提供为期一年的免费服务，具体内容视筛查项目而定。包括：</p>
-
-        <a href="{{asset('process')}}"><button type="button" class="btn btn-primary">查看检测流程</button></a>
-    </div>
-
-    <div class="bottom-padding col-sm-6 col-md-6">
-            <img src="img/tianci.jpg" class="img-responsive img-rounded center-block" alt=""/>
-    </div>
-</div>
-</div>
-
-<div class="full-width-box bottom-padding" id="tem">
-    <div class="fwb-bg background-green"></div>
-
+<div class="container-fluid" id="duyi">
     <div class="container">
-        <div class="title-box text-center title-white">
-            <h1 class="title">客户眼中的邦禾</h1>
-        </div>
-        <div>
-        <blockquote>
-          <p>应该是很准确，我在做之前也在网上查了好多资料，准确率有99.99%我才敢试试，还没拿到结果，希望不会失望吧！</p>
-          <footer>用户1</footer>
-        </blockquote>
-        <blockquote>
-          <p>前天拿到的结果，给妈妈测的，很厚的一本报告，看起来好高大上，我需要仔细研究研究。</p>
-          <footer>用户2</footer>
-        </blockquote>
-        <blockquote>
-          <p>基因检测很久之前就听说过，一直没敢试，就是怕不准，我是朋友介绍的，说邦禾的服务啊，技术啊什么的都是国内最先进的，看到报纸，感觉价格比其他公司便宜好多，抱着试试的心态给老公测了测，现在在等结果！</p>
-          <footer>用户3</footer>
-        </blockquote>
+        <div class="row" >
+            <div class="bottom-padding col-sm-6 col-md-6">
+                    <img src="img/butongren.jpg" class="img-responsive img-rounded center-block" alt=""/>
+            </div>
+
+            <div class="bottom-padding col-sm-6 col-md-6">
+                <h4>基因的独一无二性</h4>
+                <p class="lead">看看你周围的人，你可以一目了然地发现，他们之间是多么的不同。</p>
+
+                <p class="lead">他们的脸、身体、言行举止以及个性似乎都举世无双。整个人类社会当然也是如此。</p>
+            </div>
+
+
         </div>
     </div>
 </div>
@@ -81,60 +51,61 @@
 
 <div class="row">
     <div class="bottom-padding col-sm-6 col-md-6">
-        <h4>安全与隐私</h4>
-        <p>答案： <strong>6个</strong></p>
+        <h4>隐私与安全</h4>
 
-        <p>采集基因样本，进行基因筛查，出具基因报告，专业解读报告，并提供专家指导服务。</p>
-        <p>受检者收到基因报告后，由专业老师为受检者提供一对一的报告讲解：让受检者更清晰的了解自己的遗传体质特征；不同的年龄段健康或教育的重点在哪里；具体应怎样执行，如饮食怎样调理，怎样加强保健，怎样教育宝宝等。具体内容视受检者所选筛查项而定。</p>
-        <h4>广告词</h4>
-        <p>收到基因报告后，由检测机构为受检者提供为期一年的免费服务，具体内容视筛查项目而定。包括：</p>
+        <p class="lead">我们非常重视您的隐私，邦禾生物承诺不向任何人或组织透露用户个人信息，除非获得当事人授权或根据国家相关法律要求，为此邦禾生物特别制定 <strong>《隐私权和安全性声明》</strong>。</p>
 
-        <a href="{{asset('process')}}"><button type="button" class="btn btn-primary">查看检测流程</button></a>
     </div>
 
     <div class="bottom-padding col-sm-6 col-md-6">
-            <img src="img/tianci.jpg" class="img-responsive img-rounded center-block" alt=""/>
+        <img src="img/anquan.jpg" class="img-responsive img-rounded center-block" alt=""/>
+    </div>
+</div>
+</div>
+
+<div class="container-fluid" id="duyi">
+    <div class="container">
+        <div class="row" >
+            <div class="bottom-padding col-sm-6 col-md-6">
+                    <img src="img/jishu.jpg" class="img-responsive img-rounded center-block" alt=""/>
+            </div>
+
+            <div class="bottom-padding col-sm-6 col-md-6">
+                <h4>我们的技术标准</h4>
+
+            </div>
+
+
+        </div>
     </div>
 </div>
 
+<div class="full-width-box bottom-padding" id="tem">
+    <div class="fwb-bg background-green"></div>
 
-<div class="row">
-    <div class="bottom-padding col-sm-6 col-md-6">
-            <img src="img/tianci.jpg" class="img-responsive img-rounded center-block" alt=""/>
+    <div class="container">
+        <div class="title-box text-center title-white">
+            <h1 class="title">行业新闻</h1>
+        </div>
+        <div>
+        <blockquote>
+          <a href=""><p>游走在疏堵间的基因测序</p></a>
+          <footer>2014.11.26</footer>
+        </blockquote>
+        <blockquote>
+          <a href=""><p>中国科学家领衔的国际团队完成世界首个兰花全基因组测序</p></a>
+          <footer>2014.11.24</footer>
+        </blockquote>
+        <blockquote>
+          <a href=""><p>中国科学家完成水稻5个“近亲”全基因组测序</p></a>
+          <footer>2014.11.05</footer>
+        </blockquote>
+        </div>
     </div>
-
-    <div class="bottom-padding col-sm-6 col-md-6">
-        <h4>我们的技术与标准</h4>
-        <p>答案： <strong>6个</strong></p>
-
-        <p>采集基因样本，进行基因筛查，出具基因报告，专业解读报告，并提供专家指导服务。</p>
-        <p>受检者收到基因报告后，由专业老师为受检者提供一对一的报告讲解：让受检者更清晰的了解自己的遗传体质特征；不同的年龄段健康或教育的重点在哪里；具体应怎样执行，如饮食怎样调理，怎样加强保健，怎样教育宝宝等。具体内容视受检者所选筛查项而定。</p>
-        <h4>广告词</h4>
-        <p>收到基因报告后，由检测机构为受检者提供为期一年的免费服务，具体内容视筛查项目而定。包括：</p>
-
-        <a href="{{asset('process')}}"><button type="button" class="btn btn-primary">查看检测流程</button></a>
-    </div>
-
-
 </div>
 
-<div class="row">
-    <div class="title-box text-center col-sm-12 col-md-12">
-        <h1 class="title">新闻，行业新闻，公司新闻</h1>
-    </div>
-
-    <div class="bottom-padding col-sm-12 col-md-12">
-        <p>上海邦禾生物科技有限公司坐落于中国（上海）自由贸易实验区，注册资金一亿元人民币，是一家专业从事基因检测的高科技企业。</p>
-
-        <p>邦禾基因检测平台-生物芯片上海国家工程研究中心，代表着中国在生物芯片基因检测领域的最高科技权威。生物芯片上海国家工程研究中心由上海生命科学院、国家人类基因组南方中心、复旦大学、交通大学等股东单位组建而成，国家发改委委员会于2003年3月正式批复成立，总投资2.9亿元人民币，是我国投资规模最大的生物技术中心之一，也是我国在生物芯片领域研发的代表单位。</p>
-
-        <p>中心聚集众多位博士、硕士、留学归国人员等尖端科技人员，获得国家863计划、973计划、上海市重大项目等科研资助逾6000万元。</p>
-        <a href="#price"><button type="button" class="btn btn-primary">查看套餐</button></a>
-    </div>
-
-</div>
-
-<button type="button" class="btn btn-danger btn-lg center-block">现在订购</button>
+<div class="container">
+<a href="#"><h2><button type="button" class="btn btn-danger btn-lg center-block">现在订购</button></h2></a>
 
 
 </div>
